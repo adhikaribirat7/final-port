d2bca40e7e0378c3ac7e2e1fc75a0971f39c7a89
adhikaribirat7 <168667956+adhikaribirat7@users.noreply.github.com>
2024-11-06 11:04:04 +0545

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,9 @@
-import { OrbitControls, PerspectiveCamera, useAnimations, useGLTF } from "@react-three/drei";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  useAnimations,
+  useGLTF,
+} from "@react-three/drei";
 import React, { useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 
@@ -10,29 +15,32 @@ function Model() {
   // Start the animation
   useEffect(() => {
     if (actions) {
-      console.log(animations)
-      actions["Flexing_Arm_Clean"]?.play();  // Replace "AnimationName" with your actual animation name
+      console.log(animations);
+      actions["Flexing_Arm_Clean"]?.play(); // Replace "AnimationName" with your actual animation name
     }
   }, [actions]);
 
-  return <primitive object={scene} scale={[3,3,3]} position={[0,-4,0]}  />;
+  return <primitive object={scene} scale={[3, 3, 3]} position={[0, -4, 0]} />;
 }
 
 const Hero = () => {
-  
   return (
-    <section id='hero' className="min-h-screen justify-center relative items-start flex flex-col">
+    <section
+      id="hero"
+      className="min-h-screen justify-center relative items-start flex flex-col"
+    >
       <div className="w-full mx-auto flex flex-1 flex-col sm:mt-36 mt-20 sm:px-10 px-5 gap-3">
         <p className="sm:text-3xl text-2xl font-medium text-white text-center">
           Hi, I'm Birat
         </p>
         <p className="text-center lg:text-4xl text-2xl font-semibold bg-gradient-to-r from-[#BEC1CF] via-[#D5D8EA] to-[#D5D8EA] bg-clip-text text-transparent">
-          Freelance UI designer, Fullstack developer, & Data Miner. I create seamless web experiences for end-users.
+          Freelance UI designer, Fullstack developer. I create seamless web
+          experiences for end-users.
         </p>
       </div>
       <div className="h-1/2 top-1/2 absolute w-full ">
         <Canvas>
-        <PerspectiveCamera
+          <PerspectiveCamera
             makeDefault
             fov={35} // Field of view
             position={[0, 0, 5]} // Position the camera
@@ -40,8 +48,8 @@ const Hero = () => {
             far={1000} // Far clipping plane
           />
           <ambientLight intensity={1} />
-          <pointLight position={[0,0,5]} />
-          <Model  />
+          <pointLight position={[0, 0, 5]} />
+          <Model />
           <OrbitControls enableZoom={false} enableRotate={false} />
         </Canvas>
       </div>
@@ -50,5 +58,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-
